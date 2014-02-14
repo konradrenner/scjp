a60ce04275d3811e4ae1aa8e84aa7b89532f1f23
kollerb <bernd.koller82@gmail.com>
2014-02-14 10:14:33 +0100

    sfdasd

--- CodeBeispiele/src/main/java/at/arz/scjp/codebeispiele/bernd/Temp.java
+++ CodeBeispiele/src/main/java/at/arz/scjp/codebeispiele/bernd/Temp.java
@@ -3,7 +3,7 @@ package at.arz.scjp.codebeispiele.bernd;
 public class Temp {
 
 	public static void main(String[] args) {
-		System.out.println("Hallo Welt, ich bin Bernd!!!");
+		System.out.println("Hallo Welt, ich bin Bernd!!! :-)");
 
 	}
 
