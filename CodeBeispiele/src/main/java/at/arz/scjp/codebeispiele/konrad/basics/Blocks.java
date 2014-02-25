9a2f248f3ed811f5c608d40925985c150c61f432
Konrad Renner <koni.renner@gmail.com>
2014-02-25 21:36:26 +0100

    basics package für meine basics beispiele erstellt

--- CodeBeispiele/src/main/java/at/arz/scjp/codebeispiele/konrad/basics/Blocks.java
+++ CodeBeispiele/src/main/java/at/arz/scjp/codebeispiele/konrad/basics/Blocks.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package at.arz.scjp.codebeispiele.konrad;
+package at.arz.scjp.codebeispiele.konrad.basics;
 
 import java.util.Arrays;
 
