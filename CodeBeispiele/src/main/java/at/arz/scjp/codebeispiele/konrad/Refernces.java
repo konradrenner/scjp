ab7bdab31e4225c3a65b961c28098619773ee14f
Konrad Renner <koni.renner@gmail.com>
2014-02-25 21:28:23 +0100

    Typen gemacht

--- CodeBeispiele/src/main/java/at/arz/scjp/codebeispiele/konrad/Refernces.java
+++ CodeBeispiele/src/main/java/at/arz/scjp/codebeispiele/konrad/Refernces.java
@@ -49,7 +49,7 @@ public class Refernces {
         System.out.println("doSomething index:" + index);
     }
 
-    public static final void main(String[] args) {
+    public static void main(String[] args) {
         new Subclass();
         new Subclass(456);
         new Subclass(new Date(123));
--- CodeBeispiele/src/main/java/at/arz/scjp/codebeispiele/konrad/Typen.java
+++ CodeBeispiele/src/main/java/at/arz/scjp/codebeispiele/konrad/Typen.java
@@ -16,12 +16,120 @@
  */
 package at.arz.scjp.codebeispiele.konrad;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 /**
  * Verwendung und casts der primitiven Datentypen und deren Wrapper-Klassen,
- * inkl. Boxing
+ * inkl. Boxing. Weiters werden volatile und transient Modifier behandelt
  *
  * @author Konrad Renner
  */
 public class Typen {
-    //TODO
+
+    //Nur die Deklaration von volatile macht die Variable nicht Thread-safe => zB sind increment, decrement nicht atomar!
+    //Volatile bedeutet nur, dass der Objektzustand bzw. Wert nach erfolgreichen aendern eines Threads, sofort allen Threads zugaenglich gemacht wird (die Threads aktualiseren ihre internen caches)
+    private volatile int a;
+    //Diese Deklaration ist Thread-safe, weil der Zugriff auf den primitiven Datentyp boolean immer atomar ist!
+    private volatile boolean flag;
+
+    public static void main(String[] args) throws IOException {
+        
+        doTransientExample();
+
+        //Auto Boxing
+        Integer i = 10;
+        System.out.println("i:" + i);
+        //Auto Unboxing
+        int idx = i;
+        System.out.println("idx:" + idx);
+
+        //Jeder Primitive Datentyp hat in Java eine Wrapper-Klasse!
+        Boolean bool = null;
+        try {
+            boolean little = bool;
+        } catch (NullPointerException npe) {
+            System.out.println("Beim Auto Unboxing kann es zu NPE kommen!");
+        }
+        
+        Integer[] arr = new Integer[0];
+        int[] arrZwei = new int[0];
+        
+        doSomething(idx);
+        doSomething(i);
+        doSomething((short) idx);
+        doSomething(arr);
+        doSomething(arrZwei);
+        doSomething(0, 5, 6);
+
+
+        String a = "sdfd";
+        //Der folgende Code ist auskommentiert, weil er nicht kompilieren wuerde (Integer und String stehen nicht in einer Vererbungshierarchie und koennen somit mit instanceof nicth verglichen werden
+        //if(a instanceof Integer)
+        //    System.out.println("");
+    }
+
+    public static void doSomething(int in) {
+        System.out.println("int");
+    }
+
+    public static void doSomething(Object in) {
+        System.out.println("Object");
+    }
+
+    public static void doSomething(Object[] in) {
+        System.out.println("Object[]");
+    }
+
+    public static void doSomething(short in) {
+        System.out.println("short");
+    }
+
+    public static void doSomething(long[] in) {
+        System.out.println("long[]");
+    }
+
+    public static void doSomething(Integer... in) {
+        System.out.println("Integer...");
+    }
+
+
+    public static void doTransientExample() throws IOException {
+        TransientExample example = new TransientExample();
+        System.out.println("Transient Beispiel Start, " + example);
+        //Das ist das try-with-Ressources Feature von Java 7, falls bei der Pruefung so ein Code kommt, gibts Compile Fehler!!! Aber es geht hier um die Serialisierung, deshalb verwend ich das so
+        File file = new File("/home/koni/Temp/transient.example");
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+            oos.writeObject(example);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Object readObject = null;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File("/home/koni/Temp/transient.example")))) {
+            readObject = ois.readObject();
+        } catch (IOException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Transient Beispiel Ende, " + readObject);
+    }
+}
+
+class TransientExample implements Serializable {
+
+    private transient int i = 5;
+    private int x = 5;
+
+    @Override
+    public String toString() {
+        return "TransientExample{" + "i(transient value)=" + i + ", x=" + x + '}';
+    }
 }
