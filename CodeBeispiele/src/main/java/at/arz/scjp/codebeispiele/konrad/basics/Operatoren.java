33adb431b1bd8083ce6df72a4f9d4c654964fed9
Konrad Renner <koni.renner@gmail.com>
2014-03-03 20:19:45 +0100

    Operatoren

--- CodeBeispiele/src/main/java/at/arz/scjp/codebeispiele/konrad/basics/Operatoren.java
+++ CodeBeispiele/src/main/java/at/arz/scjp/codebeispiele/konrad/basics/Operatoren.java
@@ -22,5 +22,78 @@ package at.arz.scjp.codebeispiele.konrad.basics;
  * @author Konrad Renner
  */
 public class Operatoren {
-    //TODO
+    public static void main(String[] args) {
+        int i = 2;
+
+        i *= i + 1; //Equivalent mit i = i * (i+5)
+
+        System.out.println("i:" + i);
+
+        Number n = Integer.valueOf(i);
+        //if(n instanceof String){} => Compilefehler, da nicht gleiche Vererbungshierarchie
+        Object o = (Object) n;
+        if (o instanceof Integer) {
+            System.out.println("Hallo instanceof");
+        }
+
+        {
+            boolean b = o instanceof String;
+            System.out.println("o instanceof String:" + b);
+        }
+
+        //Modulo Operator
+        //Es gilt immer: 
+        //Wenn zahl % divisor = x ist, und zahl / divisor = y ist, dann gilt: zahl = divisor*y + x.
+        i = 2;
+        int j = 4;
+        System.out.println("Ganzzahlige Division (wird immer abgerundet): " + (i / j));
+        System.out.println("Divisionsrest, Modulo, Modulus: " + (i % j));
+        System.out.println("Rueckwaerts: " + (((i / j) * j) + (i % j)));
+
+
+        System.out.println("Hi, " + i + 2);
+        System.out.println(i + 2 + " HI");
+
+        //Wichtig ist hier die Unterscheidung zwischen prefix und postfix!
+        int x = 0;
+        int z = 1;
+        int y = 2;
+        if (z == x++) {
+            System.out.println("unreachable");
+        } else if (y == ++x) {
+            System.out.println("Hallo!");
+        }
+
+        //Hier ists Wurst, Erhoehung immer bevor neuem Durchlauf!
+        for (int b = 0; b < 2; ++b) {
+            System.out.println("b:" + b);
+        }
+
+        int b = z++ + ++z;
+        //int b= z++ +++z; => nicht moeglich!
+
+        o = null;
+
+        try {
+            if (o == null | o.toString().length() == 0) {
+                System.out.println("Unreachable");
+            }
+        } catch (NullPointerException e) {
+            // bei not short curcuit logischen operatoren, werden IMMER beide Seiten ausgewertet
+            System.out.println("Reachable:" + e.getMessage());
+        }
+
+        if (o == null || o.toString().length() == 0) {
+            System.out.println("Nur erster Ausdruck wurde ausgewertet");
+        }
+
+        //Bei XOR (exclusive OR) darf nur 1 der beiden Ausdruecke true sein
+        i = 5;
+        x = 6;
+
+        boolean erg = (i < x) ^ (x > i);
+        System.out.println("erg:" + erg);
+        erg = (i > x) ^ (x > i);
+        System.out.println("erg:" + erg);
+    }
 }
