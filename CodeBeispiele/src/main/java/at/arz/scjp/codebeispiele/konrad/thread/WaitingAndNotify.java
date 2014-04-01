f5e0b8d10decf64a9bc205204cfd251e0739d4f8
Konrad Renner <koni.renner@gmail.com>
2014-04-01 13:13:08 +0200

    Update WaitingAndNotify.java

--- CodeBeispiele/src/main/java/at/arz/scjp/codebeispiele/konrad/thread/WaitingAndNotify.java
+++ CodeBeispiele/src/main/java/at/arz/scjp/codebeispiele/konrad/thread/WaitingAndNotify.java
@@ -17,76 +17,96 @@
 package at.arz.scjp.codebeispiele.konrad.thread;
 
 /**
- *
+ * Ein Beispiel das wait und notify demonstiert. Diese Methoden machen nur in einem synchronized Block Sinn! Da es bei
+ * den Methoden um die Lock-Verwaltung von Threads geht!
+ * 
  * @author Konrad Renner
  */
 public class WaitingAndNotify {
 
-    static final Monitor MONITOR = new Monitor();
+	static final Monitor MONITOR = new Monitor();
 
-    public static void main(String[] args) {
-        new Thread(new Max()).start();
-        new Thread(new Moritz()).start();
-    }
+	public static void main(String[] args) {
+		new Max().start();
+		new Moritz().start();
+	}
 }
 
 class Monitor {
 
-    int zahl = 10;
-    boolean wegnehmen = true;
+	int zahl = 10;
+	boolean wegnehmen = true;
 
-    public synchronized void machWas() {
-            if (wegnehmen) {
-                zahl--;
-            } else {
-                zahl++;
-            }
+	public synchronized void machWas() {
+		System.out.println("Starte mit Thread:" + Thread.currentThread().toString() + "; zahl:" + zahl);
+		for (int i = 0; i < 12; i++) {
+			System.out.println("Aktueller Thread:" + Thread.currentThread().toString() + "; zahl:" + zahl + "; i:" + i);
+			System.out.println("wegnehmen:" + wegnehmen);
+			if (wegnehmen) {
+				zahl--;
+			} else {
+				zahl++;
+			}
 
-            if (zahl == 0) {
-                try {
-                    wegnehmen = false;
-                    wait();
-                } catch (InterruptedException ex) {
-                    System.err.println(ex.getMessage());
-                }
-            } else if (zahl == 11) {
-                wegnehmen = true;
-                notify();
-            }
-
-    }
+			System.out.println("zahl:" + zahl);
+			if (zahl == 0) {
+				try {
+					wegnehmen = false;
+					System.out.println("" + Thread.currentThread().toString() + ": Ich muss warten");
+					// Der Thread wartet jetzt, bis er von einem anderen Thread mittels notify bzw. notifyAll
+					// "aufgeweckt" wird
+					wait();
+				} catch (InterruptedException ex) {
+					System.err.println(ex.getMessage());
+				}
+			} else if (zahl == 10) {
+				wegnehmen = true;
+				System.out.println("" + Thread.currentThread().toString() + ": Jetzt darf der andere wieder");
+				// Wuerde man hier das notifyAll oder notify nicht machen, wuerde der andere Thread "ewig" warten. Dh
+				// der andere Thread macht nicht von selbst weiter
+				// Unterschied notify und notifAll: Bei notify gibt die JVM genau 1 Thread bescheid das er weiter machen
+				// kann, es ist nicht garantiert, welcher Thread das ist und kann in bestimmten Situationen zu Deadlocks
+				// fuehren!
+				notifyAll();
+				break;
+			}
+		}
+	}
 }
 
-class Max implements Runnable {
+class Max
+		extends Thread {
 
-    @Override
-    public void run() {
-        for (int i = 0; i < 20; i++) {
-            WaitingAndNotify.MONITOR.machWas();
+	@Override
+	public void run() {
 
-            System.out.println("Aktueller Thread:" + toString() + "; zahl:" + WaitingAndNotify.MONITOR.zahl);
-        }
-    }
+		WaitingAndNotify.MONITOR.machWas();
 
-    @Override
-    public String toString() {
-        return "I bin da Max";
-    }
+		System.out.println(toString() + ": Habe meine Arbeit getan");
+		System.out.println(toString() + ": Shutdown gestartet");
+		System.out.println(toString() + ": Gute Nacht");
+	}
+
+	@Override
+	public String toString() {
+		return "I bin da Max";
+	}
 }
 
-class Moritz implements Runnable {
+class Moritz
+		extends Thread {
 
-    @Override
-    public void run() {
-        for (int i = 0; i < 20; i++) {
-            WaitingAndNotify.MONITOR.machWas();
+	@Override
+	public void run() {
+		WaitingAndNotify.MONITOR.machWas();
 
-            System.out.println("Aktueller Thread:" + toString() + "; zahl:" + WaitingAndNotify.MONITOR.zahl);
-        }
-    }
+		System.out.println(toString() + ": Habe meine Arbeit getan");
+		System.out.println(toString() + ": Shutdown gestartet");
+		System.out.println(toString() + ": Gute Nacht");
+	}
 
-    @Override
-    public String toString() {
-        return "I bin da Moritz";
-    }
+	@Override
+	public String toString() {
+		return "I bin da Moritz";
+	}
 }
